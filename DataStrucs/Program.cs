223ba2d0ce3156dc47fe19233b6ad163f15f4385
hispex <hispgg@gmail.com>
2013-08-08 12:45:22 +0300

    qwe

--- DataStrucs/Program.cs
+++ DataStrucs/Program.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataStrucs
 {
-	 class Program
+	internal class Program
 	{
-		 static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			var tree = new Tree(1);
-			tree.AddSubtree(new Tree(2));
-			tree.AddSubtree(new Tree(3));
-			tree.AddSubtree(new Tree(4));
 			
-			Algorithms.BreadthFirstTraversal(tree);
-			//blabla
 
 		}
 	}
@@ -27,7 +22,7 @@ namespace DataStrucs
 		{
 			Queue<ITree> queue = new Queue<ITree>();
 			queue.Enqueue(tree);
-			while (queue.Count>0)
+			while (queue.Count > 0)
 			{
 				var t = queue.Dequeue();
 				Console.WriteLine(t.Key);
@@ -42,15 +37,282 @@ namespace DataStrucs
 
 	public interface ITree
 	{
-		object Key { get;}
-		int Degree { get; }
+		object Key { get; }
 		ITree GetSubtree(int i);
+		bool IsLeaf { get; }
+		int Degree { get; }
+		int Height { get; }
+		void DepthFirstTraversal(IPrePostVisitor visitor);
+		void BreadthFirstTraversal(Visitor visitor);
 	}
 
-	public class Tree:ITree
+	public abstract class AbstractTree : ITree
 	{
-		private List<Tree> subtrees=new List<Tree>();
-		
+		public int Degree { get; private set; }
+
+		public virtual void BreadthFirstTraversal(Visitor visitor)
+		{
+			Queue<object> queue = new Queue<object>();
+			if (!IsEmpty)
+			{
+				queue.Enqueue(this);
+			}
+			while (queue.Count > 0 && !visitor.IsDone)
+			{
+				Tree head = (Tree) queue.Dequeue();
+				visitor.Visit(head.Key);
+				for (int i = 0; i < head.Degree; i++)
+				{
+					ITree child = head.GetSubtree(i);
+					if (!child.IsLeaf)
+					{
+						queue.Enqueue(child);
+					}
+				}
+			}
+		}
+
+		protected bool IsEmpty
+		{
+			get { return Degree == 0; }
+		}
+
+		public virtual void DepthFirstTraversal(IPrePostVisitor visitor)
+		{
+			if (visitor.IsDone)
+			{
+				return;
+			}
+			if (!IsEmpty)
+			{
+				visitor.PreVisit(Key);
+				for (int i = 0; i < Degree; i++)
+				{
+					GetSubtree(i).DepthFirstTraversal(visitor);
+				}
+				visitor.PostVisit(Key);
+			}
+		}
+
+		public ITree GetSubtree(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Height { get; private set; }
+		public bool IsLeaf { get; private set; }
+		public object Key { get; private set; }
+
+		public void Accept(Visitor visitor)
+		{
+			DepthFirstTraversal(new PreOrder(visitor));
+		}
+	}
+
+	public class BinaryTree : AbstractTree
+	{
+		protected object key;
+		protected BinaryTree left;
+		protected BinaryTree right;
+
+		public BinaryTree(object key, BinaryTree left, BinaryTree right)
+		{
+			this.key = key;
+			this.left = left;
+			this.right = right;
+		}
+
+		public BinaryTree(object key) : this(key, new BinaryTree(), new BinaryTree())
+		{
+		}
+
+		public BinaryTree() : this(null, null, null)
+		{
+		}
+
+		public BinaryTree Left
+		{
+			get
+			{
+				if (IsEmpty)
+				{
+					throw new InvalidOperationException();
+				}
+				return left;
+			}
+			set { left = value; }
+		}
+
+		public BinaryTree Right
+		{
+			get
+			{
+				if (IsEmpty)
+				{
+					throw new InvalidOperationException();
+				}
+				return right;
+			}
+			set
+			{
+				right = value;
+			}
+		}
+
+		public override void DepthFirstTraversal(IPrePostVisitor visitor)
+		{
+			if (!IsEmpty)
+			{
+				visitor.PreVisit(key);
+				Left.DepthFirstTraversal(visitor);
+				visitor.InVisit(key);
+				Right.DepthFirstTraversal(visitor);
+				visitor.PostVisit(key);
+			}
+		}
+
+		public void Purge()
+		{
+			key = null;
+			left = null;
+			right = null;
+		}
+	}
+
+	public class ExpressionTree : BinaryTree
+	{
+		public ExpressionTree(char key) : base(key)
+		{
+		}
+
+		public static ExpressionTree ParsePostfix(TextReader reader)
+		{
+			Stack<ExpressionTree> stack = new Stack<ExpressionTree>();
+			int i;
+			while ((i = reader.Read()) >= 0)
+			{
+				char c = (char) i;
+				if (Char.IsLetterOrDigit(c))
+				{
+					stack.Push(new ExpressionTree(c));
+				}
+				else if (c == '+' || c == '-' || c == '*' || c == '/')
+				{
+					ExpressionTree result = new ExpressionTree(c);
+					result.Right=stack.Pop();
+					result.Left=stack.Pop();
+					stack.Push(result);
+				}
+			}
+			return stack.Pop();
+		}
+	}
+
+	#region Visitor
+
+	public interface IPrePostVisitor
+	{
+		void PreVisit(object obj);
+		void InVisit(object obj);
+		void PostVisit(object obj);
+		bool IsDone { get; }
+	}
+
+	public abstract class AbstractPreVisitor : IPrePostVisitor
+	{
+		public virtual void PreVisit(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void InVisit(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void PostVisit(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual bool IsDone
+		{
+			get { return false; }
+		}
+	}
+
+	public class PreOrder : AbstractPreVisitor
+	{
+		protected Visitor visitor;
+
+		public PreOrder(Visitor visitor)
+		{
+			this.visitor = visitor;
+		}
+
+		public override void PreVisit(object obj)
+		{
+			visitor.Visit(obj);
+		}
+
+		public override bool IsDone
+		{
+			get { return visitor.IsDone; }
+		}
+	}
+
+	public class InOrder : AbstractPreVisitor
+	{
+		protected Visitor visitor;
+
+		public InOrder(Visitor visitor)
+		{
+			this.visitor = visitor;
+		}
+
+		public override void InVisit(object obj)
+		{
+			visitor.Visit(obj);
+		}
+
+		public override bool IsDone
+		{
+			get { return visitor.IsDone; }
+		}
+	}
+
+	public class PostOrder : AbstractPreVisitor
+	{
+		protected Visitor visitor;
+
+		public PostOrder(Visitor visitor)
+		{
+			this.visitor = visitor;
+		}
+
+		public override void PostVisit(object obj)
+		{
+			visitor.Visit(obj);
+		}
+
+		public override bool IsDone
+		{
+			get { return visitor.IsDone; }
+		}
+	}
+
+	public interface Visitor
+	{
+		void Visit(object o);
+		bool IsDone { get; set; }
+	}
+
+	#endregion
+
+	public class Tree : ITree
+	{
+		private List<Tree> subtrees = new List<Tree>();
+
 		private int key;
 
 		public Tree(int key)
@@ -58,18 +320,39 @@ namespace DataStrucs
 			this.key = key;
 		}
 
-		public object Key { get { return key; }
+		public object Key
+		{
+			get { return key; }
 		}
-		public int Degree { get { return subtrees.Count; } }
+
+		public int Degree
+		{
+			get { return subtrees.Count; }
+		}
+
+		public int Height { get; private set; }
+
+		public void DepthFirstTraversal(IPrePostVisitor visitor)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BreadthFirstTraversal(Visitor visitor)
+		{
+			throw new NotImplementedException();
+		}
 
 		public ITree GetSubtree(int i)
 		{
-			if (i > subtrees.Count-1)
+			if (i > subtrees.Count - 1)
 			{
 				throw new ArgumentOutOfRangeException();
 			}
 			return subtrees[i];
 		}
+
+		public bool IsLeaf { get; private set; }
+
 		public void AddSubtree(Tree subtree)
 		{
 			subtrees.Add(subtree);
---
+++ DataStrucs/SearchTree.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucs
+{
+	public interface ISearchTree: ICollection, ITree
+{
+		IComparable Min { get; }
+		IComparable Max { get; }
+
+		//need Find()
+}
+
+	public class BinarySearchTree:BinaryTree, ISearchTree
+	{
+		new public virtual IComparable Key { get { return (IComparable) base.Key; } }
+		new public virtual BinarySearchTree Left { get { return (BinarySearchTree) base.Left; } }
+		new public virtual BinarySearchTree Right { get { return (BinarySearchTree) base.Right; } }
+
+		public virtual IComparable Find(IComparable obj)
+		{
+			if (this.IsEmpty)
+			{
+				return null;
+			}
+
+			int diff = obj.CompareTo(Key);
+			if (diff == 0)
+			{
+				return Key;
+			}
+			else if (diff < 0)
+			{
+				return Left.Find(obj);
+			}
+			else
+			{
+				return Right.Find(obj);
+			}
+		}
+	
+		public virtual IComparable Min
+		{
+			get
+			{
+				if (IsEmpty)
+					return null;
+				else if (Left.IsEmpty)
+					return Key;
+				else
+					return Left.Min;
+			}
+		}
+		public virtual IComparable Max
+		{
+			get
+			{
+				if (IsEmpty)
+					return null;
+				else if (Right.IsEmpty)
+					return Key;
+				else
+					return Right.Max;
+			}
+		}
+
+		public virtual void Remove(IComparable obj)
+		{
+			if (IsEmpty)
+			{
+				throw new ArgumentException("object not found");
+			}
+			int diff = obj.CompareTo(Key);
+			if (diff==0)
+			{
+				if (!Left.IsEmpty)
+				{
+					IComparable max = Left.Max;
+					key = max;
+					Left.Remove(max);
+				}
+				else if (!Right.IsEmpty)
+				{
+					IComparable min = Right.Min;
+					key = min;
+					Right.Remove(min);
+				}
+				else
+				{
+					//DetachKey();
+				}
+			}
+			else if (diff < 0)
+			{
+				Left.Remove(obj);
+			}
+			else
+			{
+				Right.Remove(obj);
+			}
+			Balance();
+		}
+		public virtual void Insert(IComparable obj)
+		{
+			if (IsEmpty)
+			{
+				AttachKey(obj);
+			}
+			else
+			{
+				int diff = obj.CompareTo(Key);
+				if (diff == 0)
+				{
+					throw new ArgumentException("duplicate key");
+				}
+				if (diff<0)
+				{
+					Left.Insert(obj);
+				}
+				else
+				{
+					Right.Insert(obj);
+				}
+			}
+			Balance();
+		}
+
+		protected void Balance()
+		{
+			
+		}
+		private void AttachKey(object obj)
+		{
+			if (!IsEmpty)
+			{
+				throw new InvalidOperationException();
+			}
+			key = obj;
+			left=new BinarySearchTree();
+			right=new BinarySearchTree();
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyTo(Array array, int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Count { get; private set; }
+		public object SyncRoot { get; private set; }
+		public bool IsSynchronized { get; private set; }
+	}
+
+interface ISorter
+{
+	void Sort(IComparable[] array);
+}
+
+	public abstract class AbstractQuickSorter:ISorter
+	{
+		protected const int CUTOFF = 2;
+		protected abstract int SelectPivot(int left, int right);
+
+		private IComparable[] array;
+		private int n;
+
+		protected void Sort()
+		{
+			Sort(0, n - 1);
+			
+		}
+		public void Sort(IComparable[] array)
+		{
+			n = array.Length;
+			this.array = array;
+			if (n > 0)
+			{
+				Sort();
+			}
+			this.array = null;
+		}
+
+		protected void Sort(int left, int right )
+		{
+			if (right-left+1>CUTOFF)
+			{
+				int p = SelectPivot(left, right);
+				Swap(p, right);
+				var pivot = array[right];
+				int i = left;
+				int j = right - 1;
+				for (;;)
+				{
+					while (i < j && array[i].CompareTo(pivot)<0) ++i;
+					while (i < j && array[j].CompareTo(pivot)>0) --j;
+					if (i >= j) break;
+					Swap(i++,j--);
+				}
+				if (array[i].CompareTo(pivot) > 0)
+				{
+					Swap(i,right);
+				}
+				if (left < i)
+				{
+					Sort(left,i-1);
+				}
+				if (right>i)
+				{
+					Sort(i+1,right);
+				}
+
+			}
+		}
+
+		private void Swap(int  a, int b)
+		{
+			var tmp = array[a];
+			array[a] = array[b];
+			array[b] = tmp;
+		}
+	}
+}
+
