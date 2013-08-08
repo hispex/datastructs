1c546b131cb5e865729d7e319cc1f12378d5b64b
hispex <hispgg@gmail.com>
2013-08-08 15:51:13 +0300

    graph1

---
+++ DataStrucs/Graph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucs
+{
+	class Graph
+	{
+	}
+
+	public interface IVertex: IComparable
+	{
+		int Number { get; }
+		object Weight { get; }
+		IEnumerable IncidentEdge { get; }
+		IEnumerable EmanatingEdges { get; }
+		IEnumerable Predecessors { get;  }
+		IEnumerable Successors { get; }
+	}
+	public interface IEdge:IComparable
+	{
+		IVertex VO { get; }
+		IVertex V1 { get; }
+		object Weight { get; }
+		bool IsDirected { get; }
+		IVertex MateOf(IVertex vertex);
+	}
+}
